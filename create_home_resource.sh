99fcd001c51fb20a7c133217f1b25dbf09e70944
Ashley Rine <ashley.rine@gmail.com>
2022-02-08 10:53:10 -0500

    Update create_home_resource.sh

--- create_home_resource.sh
+++ create_home_resource.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-#This script creates a resource for an employee's Owl
+#This script creates a resource for an employee's MHQ
 
 gam="$HOME/bin/gam/gam"
 
 #Enter employee name
 echo "User's first name (with no spaces):"
 read firstname
+echo "User's email address:"
+read email
 
-$gam create resource $firstname-place "$firstname's Place" type "Personal Office" category CONFERENCE_ROOM building "Homes" capacity "1" floor "1" 
+#$gam create resource $firstname-place "$firstname's Place" type "Personal Office" category CONFERENCE_ROOM building "Homes" capacity "1" floor "1" 
+
+$gam info resource $firstname-place | grep -i "resourceEmail:" 
+
+echo "Copy/paste the resource email:"
+read resourceemail
+
+$gam calendar $resourceemail add editor $email
+
